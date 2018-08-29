ae9953774a0975fbbb4f2b5a495f7e1f04d4ed26
Jeremy Stribling <strib@alum.mit.edu>
2018-08-29 13:34:00 -0700

    ops: track "uncommitted" unrefs in `resolutionOp`
    When we run through conflict resolution, sometimes we need to unref
    blocks that have been pushed to the bserver, but not actually
    committed as part of any MD because _another_ conflict was hit before
    we could put the MD.  These blocks are special because they still need
    to be marked as unreferenced in the next successful PR (so they can be
    archived/deleted from the bserver), but without actually counting
    their bytes toward the TLF byte usage stored in the MD update.
    
    So this PR introduces a new field in `resolutionOp` to track these
    blocks separately, so we can tell whether or not to count their bytes
    on a future resolution.
    
    Issue: KBFS-3303

--- libkbfs/folder_update_prepper.go
+++ libkbfs/folder_update_prepper.go
@@ -650,7 +650,7 @@ func addUnrefToFinalResOp(ops opsList, ptr BlockPointer,
 		resOp = newResolutionOp()
 		ops = append(ops, resOp)
 	}
-	resOp.AddUnrefBlock(ptr)
+	resOp.AddUncommittedUnrefBlock(ptr)
 	return ops
 }
 
@@ -707,6 +707,19 @@ func (fup *folderUpdatePrepper) updateResolutionUsageAndPointersLockedCache(
 				unrefs[update.Unref] = true
 				delete(refs, update.Unref)
 				refs[update.Ref] = true
+			}
+		}
+	}
+
+	for _, resOp := range unmergedChains.resOps {
+		for _, ptr := range resOp.CommittedUnrefs() {
+			original, err := unmergedChains.originalFromMostRecentOrSame(ptr)
+			if err != nil {
+				return nil, err
+			}
+			if !unmergedChains.isCreated(original) {
+				fup.log.CDebugf(ctx, "Unref'ing %v from old resOp", ptr)
+				unrefs[ptr] = true
 			}
 		}
 	}
